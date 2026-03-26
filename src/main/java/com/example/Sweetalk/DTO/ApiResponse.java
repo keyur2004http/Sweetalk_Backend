bffdf7f7a6bb433a0b539a11e64e89b81b6d611f
keyur2004http <kidechakeyur@123gmail.com>
2026-03-26 18:39:58 +0530

    fix :cors

---
+++ src/main/java/com/example/Sweetalk/DTO/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.example.Sweetalk.DTO;
+
+public class ApiResponse {
+    private boolean success;
+    private String message;
+
+    public ApiResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+    // Getters and Setters
+    public boolean isSuccess() { return success; }
+    public String getMessage() { return message; }
+}
